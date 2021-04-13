b78ef3b068b3a527225255d8a898ca6a377d4f66
limyoutaek <atrlkwqr@gmail.com>
2021-04-13 15:55:56 +0900

    add body for css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
+import Body from "./Body";
 import Routes from "./Routes";
 
 class App extends Component {
@@ -8,7 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header></Header>
-        <Routes></Routes>
+        <Body>
+          <Routes></Routes>
+        </Body>
         <Footer></Footer>
       </div>
     )
---
+++ src/Components/Body.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Body = styled.div`
+    width: 100%;
+    height: max-content;
+`;
+
+
+export default Body;
