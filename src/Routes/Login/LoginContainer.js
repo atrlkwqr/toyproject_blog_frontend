023cb8a65bc9ecd7336175dd61688155a27dcd59
limyoutaek <atrlkwqr@gmail.com>
2021-04-13 17:49:14 +0900

    add login form

---
+++ src/Routes/Login/LoginContainer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import LoginPresenter from "./LoginPresenter";
+
+
+class LoginContainer extends Component {
+    state = {
+        loading: true
+    };
+    componentDidMount() {
+        fetch('/BackEndLoginApi')
+        .then(res => res.json())
+        .then(
+            posts => this.setState({
+                loading: false,
+                posts
+            }),
+            error => this.setState({
+                loading: false,
+                error
+            })
+        );
+    }
+    render() {
+        <LoginPresenter { ...this.state}></LoginPresenter>;
+    }
+
+}
+
+export default LoginContainer;
--- src/Routes/Login/LoginPresenter.js
+++ src/Routes/Login/LoginPresenter.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+import Input from "../../Components/Input";
+import Page404 from "../../Components/Page404"
 
-class Login extends Component {
+
+const LoginBox = styled.div`
+    background-color: yellow;
+`;
+
+class LoginPresenter extends Component {
+    _renderLoading() {
+        return (
+            <div>Loading...</div>
+        )
+    }
+
+    _renderError() {
+        return <Page404></Page404>
+    }
+
+    _renderLogin() {
+        return (
+            //<Input placeholder="email" {...email} />
+            //<Input placeholder="password" {...password} />
+            <React.Fragment>
+            <LoginBox>
+                <Input></Input>
+            </LoginBox>
+            <LoginBox>
+                <Input></Input>
+            </LoginBox>
+            </React.Fragment>
+        )
+    }
+
     render(){
-      return (
-        <h1>Login</h1>
-      );
+    //   if(this.props.loading) {
+    //       return this._renderLoading();
+    //   } else if(this.props.posts) {
+    //       return this._renderLogin();
+    //   } else {
+    //       return this._renderError();
+    //   }
+       return this._renderLogin();
     }
   }
 
-export default Login;
+export default LoginPresenter;
