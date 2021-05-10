d8ea7d59c9885f71c5cb504a26bfcc0c23210fb2
limyoutaek <atrlkwqr@gmail.com>
2021-05-10 16:58:15 +0900

    add fileserver

--- src/Routes/PostWrite/PostWriteContainer.js
+++ src/Routes/PostWrite/PostWriteContainer.js
@@ -1,43 +1,66 @@
 import React, {useState} from "react";
 import PostWritePresenter from "./PostWritePresenter";
 import {useMutation} from "@apollo/react-hooks";
-import { toast } from 'react-toastify';
-import { WRITE } from './PostWriteQuerie';
-import { useInput } from "../../utils";
+import {toast} from 'react-toastify';
+import {WRITE} from './PostWriteQuerie';
+import {useInput} from "../../utils";
+import axios from "axios";
 
 const PostWriteContainer = () => {
 
     const editorRef = React.createRef();
 
     const title = useInput("");
+    const [fileData, setFileData] = useState(null);
     const [submitting, setSubmitting] = useState(false);
 
-    const [writePostMutation, {loading}] = useMutation(WRITE);
+    const [writePostMutation, {
+            loading
+        }
+    ] = useMutation(WRITE);
 
     const clickFunc = async (e) => {
         setSubmitting(true);
         e.preventDefault();
 
-        const contents = editorRef.current.getInstance().getHtml()
+        const contents = editorRef
+            .current
+            .getInstance()
+            .getHtml()
 
-        const {
-            data: {
-                writePost: writePostResponse
-            }
-        } = await writePostMutation({variables: {
-                title:title.value,
-                contents: contents
-        }});
+        // const {     data: {         writePost: writePostResponse     } } = await
+        // writePostMutation({variables: {         title:title.value,         contents:
+        // contents }});
 
-        if(!loading){
-            console.log(writePostResponse)
-            if(writePostResponse===true){
-                toast("Write Success!")
-                setTimeout(function(){ window.location.href="/" }, 4000);
-            } else {
-                setSubmitting(false);
-                toast("Error!")
+        if (contents !== undefined && contents !== null) {
+            const jwt = localStorage.getItem("token");
 
+            const formData = new FormData();
+            formData.append("title", title.value);
+            formData.append("streamfile", contents);
+            formData.append("jwt", jwt);
+
+            const writePostResponse = await axios({
+                method: "post",
+                url: "http://localhost:5000/uploads",
+                data: formData,
+                headers: {
+                    Authorization: jwt,
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            if (!loading) {
+
+                if (writePostResponse.status === 200) {
+                    toast("Write Success!")
+                    setTimeout(function () {
+                        window.location.href = "/"
+                    }, 4000);
+                } else {
+                    setSubmitting(false);
+                    toast("Error!")
+
+                }
             }
         }
     };
@@ -45,14 +68,12 @@ const PostWriteContainer = () => {
     const handleFocus = () => {
         console.log('focus!!');
     };
-    
 
     return <PostWritePresenter
-    editorRef={editorRef}
-    clickFunc={clickFunc}
-    handleFocus={handleFocus}
-    title={title}></PostWritePresenter>;
+        editorRef={editorRef}
+        clickFunc={clickFunc}
+        handleFocus={handleFocus}
+        title={title}></PostWritePresenter>;
 }
-
 
 export default PostWriteContainer;
